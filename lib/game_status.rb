33dbeddf4815b450882b13beab24ee49fc88ddd1
Nicholas Stano <nicholasstano@gmail.com>
2019-02-15 23:16:55 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -2,8 +2,6 @@
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
-
-<<<<<<< HEAD
 WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]]
 
 def won?(board)
@@ -31,39 +29,3 @@ if winning_combo = won?(board)
   board[winning_combo.first]
 end
 end
-=======
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
-top_row_win = [0, 1, 2]
-if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
-  "X won in the top row"
-end
-
-# Define your WIN_COMBINATIONS constant
-
-WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]]
-
-def won?(board)
-for each win_combination in WIN_COMBINATIONS
-  # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
-  # grab each index from the win_combination that composes a win.
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
- 
-  position_1 = board[win_index_1] # load the value of the board at win_index_1
-  position_2 = board[win_index_2] # load the value of the board at win_index_2
-  position_3 = board[win_index_3] # load the value of the board at win_index_3
- 
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination # return the win_combination indexes that won.
-  else
-    false
-  end
-end
-end
-
-def draw?(board)
-end
-
-
->>>>>>> 319af96af1bbb151f360560787b62e46babaad03
